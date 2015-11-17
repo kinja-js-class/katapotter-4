36d734e1073498172cc5d3d4ba924bdbf9449ebe
Gáspár Körtesi <gazs@gawker.com>
2015-11-17 10:45:09 +0100

    add stupid test

--- spec/test.js
+++ spec/test.js
@@ -1,1 +1,8 @@
-const stuff = {};
+//this is a check that tests that one book costs €8
+
+// given _ when _ then
+describe("A books", () => {
+	it("when i buy 1 book, then i pay €8", () => {
+		expect(7).toBe(8);
+	})
+})
