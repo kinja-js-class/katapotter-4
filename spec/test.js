8844cc0d08ca2ca2406f5e1f396492e768e8427c
Gáspár Körtesi <gazs@gawker.com>
2015-11-24 10:33:10 +0100

    2different books

--- spec/test.js
+++ spec/test.js
@@ -1,4 +1,3 @@
-// priceCalculator([1,2,3,4]) => finalPrice
 import {priceCalculator} from '../../dist/app';
 
 describe('priceCalculator', () => {
@@ -7,11 +6,27 @@ describe('priceCalculator', () => {
 	});
 
 	it('Buying 1 book costs 8', () => {
-		let books = [
+		let cart = [
 			{
 			price: 8
 		}
 		];
-		expect(priceCalculator(books)).toBe(8);
+		expect(priceCalculator(cart)).toBe(8);
+	});
+	it('Buying 2 of the same book costs 16', () => {
+		let cart = [
+			{isbn: '1', price: 8},
+			{isbn: '1', price: 8}
+		];
+		expect(priceCalculator(cart)).toBe(16);
+	});
+	it('Buying 2 different costs 16 * 0.95', () => {
+		let cart = [
+			{isbn: '1', price: 8},
+			{isbn: '2', price: 8}
+		];
+		expect(priceCalculator(cart)).toBe(16 * 0.95);
 	});
 })
+
+
--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
-let priceCalculator = (books) => {
-	if (books) {
-		return 8;
+let priceCalculator = (cart) => {
+	if (cart) {
+		if (cart.length == 2 && cart[0].isbn != cart[1].isbn) {
+			return 16 * 0.95;
+		} else {
+			return 8 * cart.length;
+		}
 	} else {
 		return 0;
 	}
