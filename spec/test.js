4ec4a833c288aea8625e9287ec033280ca12c0c8
Gáspár Körtesi <gazs@gawker.com>
2015-11-24 10:04:53 +0100

    let's rewrite everything

--- spec/test.js
+++ spec/test.js
@@ -1,26 +1,8 @@
-//this is a check that tests that one book costs €8
-import {priceCalculator, books} from '../../dist/app';
+// priceCalculator([1,2,3,4]) => finalPrice
+import {priceCalculator} from '../../dist/app';
 
-// given _ when _ then
-describe("A books", () => {
-	it("when i buy 1 book, then i pay €8", () => {
-		let booksToBuy = [books[0]];
-		expect(priceCalculator(booksToBuy)).toBe(8);
-	})
-	it("when i buy 2 different books, then i pay (2 * €8) * 0.95 = €15.2", () => {
-		let booksToBuy = [books[0], books[1]];
-		expect(priceCalculator(booksToBuy)).toBe(15.2);
-	})
-	it("when i buy 3 different books, then i pay (3 * €8) * 0.90 = €21.6", () => {
-		let booksToBuy = [books[0], books[1], books[2]];
-		expect(priceCalculator(booksToBuy)).toBe(21.6);
-	})
-	it("when i buy 4 different books, then i pay (4 * €8) * 0.80 = €25.6", () => {
-		let booksToBuy = [books[0], books[1], books[2], books[3]];
-		expect(priceCalculator(booksToBuy)).toBe(25.6);
-	})
-	it("when i buy 5 different books, then i pay (5 * €8) * 0.75 = €30", () => {
-		let booksToBuy =  [books[0], books[1], books[2], books[3], books[4]];
-		expect(priceCalculator(booksToBuy)).toBe(30);
+describe('priceCalculator', () => {
+	it('called with no arguments', () => {
+		expect(priceCalculator()).toBe(0)
 	})
 })
--- src/app.js
+++ src/app.js
@@ -1,21 +1,3 @@
-let books = [ 
-	{price: 8}, 
-	{price: 8}, 
-	{price: 8}, 
-	{price: 8}, 
-	{price: 8}
-];
+let priceCalculator = () => 0
 
-let discounts = {
-        1: 1,
-        2: 0.95,
-        3: 0.90,
-        4: 0.80,
-        5: 0.75
-    }
-
-let priceCalculator = (books) => {
-	return books.reduce((previous, current) => previous + current.price, 0) * discounts[books.length];
-};
-
-export { books, discounts, priceCalculator };
+export {priceCalculator}
